3ae5cc5ff6af35feec6c11c31e7e6ab1689c4fcc
juan pablo javier leon leon <jleon@larrain.labs.inf.utfsm.cl>
2018-12-20 21:09:44 -0300

    se ajusto el processinterface para que tuviese los metodos para hacer la comunicación entre el representante y vecinos, se agregaron los metodos de createTimerTaskVecino, createrTimerTaskRepresentante, ReducirCounter y NotificarVecinos

--- ProcessInterface.java
+++ ProcessInterface.java
@@ -6,4 +6,6 @@ interface ProcessInterface extends Remote {
     //public void Election(int callerMaxID, int callerID, int initID) throws Exception;
     //public void Echo(int callerMaxID, int callerID, int initID) throws Exception;
     public void SendText(int callerID, String text) throws Exception;
+    public void NotificarVecinos() throws Exception;
+    public void ReducirCounter() throws Exception;
 }
